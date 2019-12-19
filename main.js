c47e66bbaf601f9a58e76cba4952c15536845e22
Shruti <shrutihirpara@gmail.com>
2019-12-19 10:29:26 -0500

    fourth

--- main.js
+++ main.js
@@ -1,65 +1,49 @@
 var express= require('express');
-console.log("starting");
 var app= express();
 var bodyParser = require('body-parser');
 const util= require('util');
 var exec = util.promisify(require('child_process').exec);
 var outputString;
 
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    if('OPTIONS'==req.method)  {
-        res.sendStatus(200);
-    }
-    else{
-        next();
-    }
-});
 app.use(bodyParser.json());
-
-
-app.get('/buildPLB',async function(req,res,next){
-    const {stdout,stderr} = await exec("~/Desktop/second.sh");
-    outputString=stdout.toString(); 
-    console.log("output from the script: "+outputString);
-    const lines = outputString.split(/\r?\n/);
-    lines.forEach((line) => {
-        if(line.search( "FINAL_URL2" )!=-1){
-        var length= "FINAL_URL".length;
-        var url=line.slice(length+1);
-        console.log(url);
-        // res.send(outputString);
-    }
-       
-    });
-    res.send("no url found");
-  }); 
-
-  app.get('/test',async function(req,res,next){
-    console.log("testing");
-    res.send("testing");
-    
-  }); 
-
 app.listen(3000);
 async function call(req,res,next){
-const {stdout,stderr} = await exec("~/Desktop/second.sh");
-outputString=stdout.toString(); 
-console.log("output from the script: "+outputString);
-const lines = outputString.split(/\r?\n/);
-lines.forEach((line) => {
-    if(line.search( "FINAL_URL" )!=-1){
-    var length= "FINAL_URL".length;
-    var url=line.slice(length+1);
-    console.log(url);
-    // res.send(outputString);
+    const {stdout,stderr} = await exec("~/Desktop/second.sh");
+    outputString= stdout.toString();
+    console.log("output");
+    console.log("___________________________");
+    console.log(stdout.toString());
+    console.log("___________________________");
+    console.log("error:");
+    console.log("___________________________");
+    console.log(stderr.toString());
+    console.log("------------Done------------");
+    
+    
 }
-   
-});
-console.log("no url found");
-}
+// async function call(req,res,next){
+//     const {stdout,stderr} = await exec("~/Desktop/second.sh");
+//     outputString=stdout.toString(); 
+//     console.log("output from the script: ");
+//     console.log("___________________________");
+//     console.log(outputString);
+//     console.log("___________________________");
+//     const lines = outputString.split(/\r?\n/);
+//     var url="";
+//     var findUrl="FINAL_URL";
+//         lines.forEach((line) => {
+//             if(line.search(findUrl)!=-1){
+//             url=line.slice(findUrl.length+1);     
+//         }  
+//         });
+//     if(url===""){
+//         console.log("no url found");
+//     }
+//     else{
+//         console.log("found the url::::: "+url);
+//     }
+
+// }
 call();
 
 process.on("unhandledRejection", (reason, p) => {
